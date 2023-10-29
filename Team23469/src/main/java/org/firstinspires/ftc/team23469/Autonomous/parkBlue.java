8abf486d696d6ba26e326ad13bb61b089ec0eb51
george mitchom <george.a.mitchom@gmail.com>
2023-10-29 00:05:11 -0500

    added new folder for 23469

---
+++ Team23469/src/main/java/org/firstinspires/ftc/team23469/Autonomous/parkBlue.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.team23469.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.team23469.robot.utilities.DriveUtil2023;
+import org.openftc.apriltag.AprilTagDetection;
+
+//@TeleOp
+@Autonomous(name="Simple Autonomous", group="Autonomous")
+public class parkBlue extends LinearOpMode
+{
+    DriveUtil2023 drive = new DriveUtil2023(this);
+    static final double FEET_PER_METER = 3.28084;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        /* initialize the robot hardware */
+        drive.init(hardwareMap);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        runtime.reset();
+
+        /* ****************************************************
+        P3- Your autonomous drive code would start right here
+        ******************************************************/
+        /* Actually do something useful */
+        while (opModeIsActive() ) {
+            parkonbluebackstage();
+        }
+    }//program end
+
+
+
+    public void parkonbluebackstage(){
+            drive.driveRobotDistanceForward(15,.5);
+    }
+}
