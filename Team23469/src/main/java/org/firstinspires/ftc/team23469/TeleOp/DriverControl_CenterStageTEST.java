fc601925dcc5df576806a983e0410e6a3b26f8bf
geargirlsftc@gmail.com <geargirlsftc@gmail.com>
2023-11-15 19:57:32 -0600

    added a hanger util to program and hanger test

---
+++ Team23469/src/main/java/org/firstinspires/ftc/team23469/TeleOp/DriverControl_CenterStageTEST.java
@@ -0,0 +1,136 @@
+/* P3 2021-22 season Driver Control Program
+ Hardware Names:
+ Webcam
+ Front_Left
+ Front_Right
+ Rear_Left
+ Rear_Right
+
+ */
+
+package org.firstinspires.ftc.team23469.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.team23469.robot.utilities.DriveUtil2023;
+import org.firstinspires.ftc.team23469.robot.utilities.HangerUtil;
+import org.firstinspires.ftc.team23469.robot.utilities.launcherUtil;
+
+
+@TeleOp(name="Driver Control Center Stage", group="Teleop")
+public class DriverControl_CenterStageTEST extends LinearOpMode {
+    DriveUtil2023 drive = new DriveUtil2023(this);
+    launcherUtil launcher = new launcherUtil(this);
+    HangerUtil hanger = new HangerUtil(this);
+
+    int temp = 1;
+    double DRIVE_SPEED = 1;
+    double handOffset   = 0;
+    //subclass is replacing inherited behavior.
+    @Override
+
+    //Initialize and run program
+    public void runOpMode() throws InterruptedException {
+        //update status on driver station
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        //init external hardware classes
+        drive.init(hardwareMap);
+        launcher.init();
+
+        //default drive move to 1 (arcade)
+        int driveMode = 1;
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        /***************************************************************
+         * Game Execution Area
+         * Runs continous until "STOP" pressed on Driver Station
+         ***************************************************************/
+        while (opModeIsActive()) {
+
+            //Set driver speed as a percentage of full (normally set to full)
+            if (gamepad1.right_bumper & gamepad1.left_bumper) {
+                DRIVE_SPEED = .25;
+            } else if (gamepad1.left_bumper) {
+                DRIVE_SPEED = .50;
+            } else if (gamepad1.right_bumper) {
+                DRIVE_SPEED = 1.00;
+            } else {
+                DRIVE_SPEED = .80;
+            }
+
+            if (gamepad1.x) {
+                drive.driveRobotStrafeLeft(1.0);
+            }
+            if (gamepad1.b) {
+                drive.driveRobotStrafeRight(1.0);
+            }
+
+
+            /***************************************************************
+             * Set Drive Mode
+             * This section of code will allow the driver to set
+             * the drive mode between arcade drive and tank drive
+             * arcade drive is set when clicking the start button on the controller
+             * tank drive is set when clicking the back button on the controller
+             * this can be done while in teleop operation
+             * default drive mode is arcade drive
+             ***************************************************************/
+            if (gamepad1.start) driveMode = 1;
+            if (gamepad1.back) driveMode = 2;
+
+            //call the respective drive mode
+            if (driveMode == 1) {
+                arcadeDrive();
+                telemetry.addData("Drive Mode", "Arcade");
+            }
+            else if (driveMode == 2) {
+                tankDrive();
+                telemetry.addData("Drive Mode", "Tank");
+            }
+            else {
+                arcadeDrive();
+                telemetry.addData("Drive Mode", "Arcade");
+            }
+
+            /***end of set drive mode code */
+            doLauncher();
+
+        } //end OpModeIsActive
+    }  //end runOpMode
+
+    public void tankDrive() {
+        drive.tankDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_stick_y, DRIVE_SPEED);
+    }
+
+    public void arcadeDrive() {
+        drive.arcadeDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_stick_y, DRIVE_SPEED);
+    }
+    public void doLauncher() {
+        // Use gamepad left & right Bumpers to open and close the claw
+        // Use the SERVO constants defined in RobotHardware class.
+        // Each time around the loop, the servos will move by a small amount.
+        // Limit the total offset to half of the full travel range
+        if (gamepad2.left_bumper) {
+            launcher.setLauncherUp();
+
+        } else {// continue looping//
+        }
+
+        if (gamepad2.right_bumper) {
+            launcher.setLauncherDown();
+        } else {// continue looping
+        }
+    }
+
+    public void doHanger() {
+        if (gamepad2.dpad_up)hanger.raiseToPosition(2, 0.25);
+        if (gamepad2.dpad_down)hanger.raiseToPosition(1, 0.25);
+        if (gamepad2.dpad_left)hanger.raiseToPosition(0, 0.25);
+    }
+} //end program
