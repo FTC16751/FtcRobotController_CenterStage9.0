b59a9f84623dceca70a3babb0b6d9ffbf8a2669e
george mitchom <george.a.mitchom@gmail.com>
2023-12-30 10:04:05 -0600

    Updated ArmUtil for christmas bot

--- Team16751/src/main/java/org/firstinspires/ftc/team16751/TeleOp/DriverControl_CenterStage.java
+++ Team16751/src/main/java/org/firstinspires/ftc/team16751/TeleOp/DriverControl_CenterStage.java
@@ -35,11 +35,13 @@ public class DriverControl_CenterStage extends LinearOpMode {
     RevBlinkinLedDriver lights;
     int temp = 1;
     double DRIVE_SPEED = 1;
+    //default drive move to 1 (arcade)
+    int driveMode = 1;
     double handOffset   = 0;
 
-    LauncherUtil launcherUtil;
+   // LauncherUtil launcherUtil;
     boolean aButtonState = false;
-    //subclass is replacing inherited behavior.
+
     @Override
 
     //Initialize and run program
@@ -52,10 +54,7 @@ public class DriverControl_CenterStage extends LinearOpMode {
         drive.init(hardwareMap);
         armUtil.init(hardwareMap);
         claw.init(hardwareMap);
-        launcherUtil = new LauncherUtil(hardwareMap);
-
-        //default drive move to 1 (arcade)
-        int driveMode = 1;
+       // launcherUtil = new LauncherUtil(hardwareMap);
 
 
         // Wait for the game to start (driver presses PLAY)
@@ -67,90 +66,106 @@ public class DriverControl_CenterStage extends LinearOpMode {
          ***************************************************************/
         while (opModeIsActive()) {
 
-
-            //Set driver speed as a percentage of full (normally set to full)
-            if (gamepad1.right_bumper & gamepad1.left_bumper) {
-                DRIVE_SPEED = .25;
-            } else if (gamepad1.left_bumper) {
-                DRIVE_SPEED = .50;
-            } else if (gamepad1.right_bumper) {
-                DRIVE_SPEED = 1.00;
-            } else {
-                DRIVE_SPEED = .80;
-            }
-
-            if (gamepad1.x) {
-                drive.driveRobotStrafeLeft(1.0);
-            }
-            if (gamepad1.b) {
-                drive.driveRobotStrafeRight(1.0);
-            }
-            //Controls for Pre-set arm locations
-            if(gamepad2.dpad_up){
-                armUtil.raiseToPosition(3, 0.5);
-            }
-            //LowScore
-            if(gamepad2.dpad_down){
-                armUtil.raiseToPosition(1,0.5);
-            }
-            //Resting
-            if(gamepad2.dpad_right){
-                armUtil.raiseToPosition(2,0.5);
-            }
-            //Acquire
-            if(gamepad2.dpad_left){
-                armUtil.raiseToPosition(4,0.5);
-            }
-            //Transport (essentially the same as resting)
-            if(gamepad2.left_bumper){
-                claw.setClawOpen();
-            }
-            if(gamepad2.right_bumper){
-                claw.setClawClosed();
-            }
-            if(gamepad2.left_trigger>0.5){
-                claw.openLeftHand();
-            } else{
-
-            }
-            if(gamepad2.right_trigger>0.5){
-                claw.openRightHand();
-
-            }
-
-
-            /***************************************************************
-             * Set Drive Mode
-             * This section of code will allow the driver to set
-             * the drive mode between arcade drive and tank drive
-             * arcade drive is set when clicking the start button on the controller
-             * tank drive is set when clicking the back button on the controller
-             * this can be done while in teleop operation
-             * default drive mode is arcade drive
-             ***************************************************************/
-            if (gamepad1.start) driveMode = 1;
-            if (gamepad1.back) driveMode = 2;
-
-            //call the respective drive mode
-            if (driveMode == 1) {
-                arcadeDrive();
-                telemetry.addData("Drive Mode", "Arcade");
-            }
-            else if (driveMode == 2) {
-                tankDrive();
-                telemetry.addData("Drive Mode", "Tank");
-            }
-            else {
-                arcadeDrive();
-                telemetry.addData("Drive Mode", "Arcade");
-            }
-
-            /***end of set drive mode code */
+            doDriverControls();
+            doArmControls();
+            // Handle arm movement based on ArmUtil's state
+            handleArmState();
+            doClawControls();
             dotelemetry();
             dolauncher();
 
         } //end OpModeIsActive
     }  //end runOpMode
+
+
+
+    private void doDriverControls() {
+        //Set driver speed as a percentage of full (normally set to full)
+        if (gamepad1.right_bumper & gamepad1.left_bumper) {
+            DRIVE_SPEED = .25;
+        } else if (gamepad1.left_bumper) {
+            DRIVE_SPEED = .50;
+        } else if (gamepad1.right_bumper) {
+            DRIVE_SPEED = 1.00;
+        } else {
+            DRIVE_SPEED = .80;
+        }
+
+        if (gamepad1.x) {
+            drive.driveRobotStrafeLeft(1.0);
+        }
+        if (gamepad1.b) {
+            drive.driveRobotStrafeRight(1.0);
+        }
+        /***************************************************************
+         * Set Drive Mode
+         * This section of code will allow the driver to set
+         * the drive mode between arcade drive and tank drive
+         * arcade drive is set when clicking the start button on the controller
+         * tank drive is set when clicking the back button on the controller
+         * this can be done while in teleop operation
+         * default drive mode is arcade drive
+         ***************************************************************/
+        if (gamepad1.start) driveMode = 1;
+        if (gamepad1.back) driveMode = 2;
+
+        //call the respective drive mode
+        if (driveMode == 1) {
+            arcadeDrive();
+            telemetry.addData("Drive Mode", "Arcade");
+        }
+        else if (driveMode == 2) {
+            tankDrive();
+            telemetry.addData("Drive Mode", "Tank");
+        }
+        else {
+            arcadeDrive();
+            telemetry.addData("Drive Mode", "Arcade");
+        }
+        /***end of set drive mode code */
+    }
+
+    private void doClawControls() {
+        if(gamepad2.left_bumper){
+            claw.setClawOpen();
+        }
+        if(gamepad2.right_bumper){
+            claw.setClawClosed();
+        }
+        if(gamepad2.left_trigger>0.5){
+            claw.openLeftHand();
+        } else{
+
+        }
+        if(gamepad2.right_trigger>0.5){
+            claw.openRightHand();
+
+        }
+    }
+
+    private void doArmControls() {
+        //Controls for Pre-set arm locations
+        //Resting
+        if(gamepad2.dpad_down){
+            armUtil.setCurrentState(ArmUtil.ArmState.INIT);
+        }
+        //transport
+        if(gamepad2.dpad_left){
+            armUtil.setCurrentState(ArmUtil.ArmState.TRANSPORT);
+        }
+        //LowScore
+        if(gamepad2.dpad_up){
+            armUtil.setCurrentState(ArmUtil.ArmState.LOW_SCORE);
+        }
+        //high score
+        if(gamepad2.dpad_right){
+            armUtil.setCurrentState(ArmUtil.ArmState.HIGH_SCORE);
+        }
+
+    }
+    private void handleArmState() {
+        armUtil.runStateMachine();
+    }
 
     public void tankDrive() {
         drive.tankDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_stick_y, DRIVE_SPEED);
@@ -162,6 +177,7 @@ public class DriverControl_CenterStage extends LinearOpMode {
 
     public void dolauncher()
     {
+        /*
         if(gamepad2.y && !launcherUtil.getToggleState()) {
             launcherUtil.toggleServo();
         }
@@ -178,6 +194,8 @@ public class DriverControl_CenterStage extends LinearOpMode {
         } else if (!gamepad2.a) {
             aButtonState = false;
         }
+
+         */
     }
     public void dotelemetry() {
         telemetry.addLine("Telemetry");
